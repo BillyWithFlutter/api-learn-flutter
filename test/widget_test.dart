857036e697f64bcf6c7eb21df53189a9d9ef9fff
billywithflutter <billy89square@gmail.com>
2023-02-17 19:18:37 +0700

    should be working fine

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,15 +2,14 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:crypto/crypto.dart';
+import 'package:intl/intl.dart';
 
-// Function to generate the BRI-Signature header value
-String generateSignature(String method, String url, String timestamp,
-    String payload, String secret) {
-  // Concatenate the request components as follows:
-  // [HTTP method]\n[URL]\n[BRI-Timestamp header value]\n[request payload]
-  String message = '$method\n$url\n$timestamp\n$payload';
+String generateSignature(String path, String verb, String token,
+    String timestamp, String body, String secret) {
+  String message =
+      'path=$path&verb=$verb&token=$token&timestamp=$timestamp&body=$body';
+  print(message);
 
-  // Generate a HMAC-SHA256 hash of the message using the consumer secret as the key
   var key = utf8.encode(secret);
   var bytes = utf8.encode(message);
   var hmacSha256 = Hmac(sha256, key);
@@ -28,6 +27,8 @@ void main() async {
   const String tokenUrl =
       'https://sandbox.partner.api.bri.co.id/oauth/client_credential/accesstoken?grant_type=client_credentials';
   const String baseUrl = 'https://sandbox.partner.api.bri.co.id/v1/briva';
+  String path = '/v1/briva';
+  String verb = 'POST';
 
   final resp = await http.post(
     Uri.parse(tokenUrl),
@@ -40,14 +41,11 @@ void main() async {
   if (resp.statusCode == 200) {
     final body = json.decode(resp.body);
     final accessToken = body['access_token'];
-    var time = DateTime.now().toIso8601String();
 
-    final headers = {
-      'content-type': 'application/json',
-      'BRI-Timestamp': time,
-      'BRI-Signature': 'GP93XAUaymaAxlHE',
-      'Authorization': 'Bearer $accessToken',
-    };
+    DateTime localTime = DateTime.now();
+    DateTime utcTime = localTime.toUtc();
+    String timestamp =
+        DateFormat('yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\'').format(utcTime);
 
     final data = jsonEncode({
       "institutionCode": "J104408",
@@ -56,22 +54,27 @@ void main() async {
       "nama": "John Doe",
       "amount": "20000",
       "keterangan": "",
-      "expiredDate": "2023-10-29 09:57:26"
+      "expiredDate": "2023-02-29 09:57:26"
     });
 
-    String signature = generateSignature(
-        'POST',
-        'https://sandbox.partner.api.bri.co.id/v1/briva',
-        time,
-        data,
-        consumerSecret);
-    headers['BRI-Signature'] = signature;
+    String token = 'Bearer$accessToken';
+    String signature =
+        generateSignature(path, verb, token, timestamp, data, consumerSecret);
+
+    final headers = {
+      'content-type': 'application/json',
+      'Authorization': 'Bearer $accessToken',
+      'BRI-Timestamp': timestamp,
+      'BRI-Signature': signature,
+    };
 
     final response = await http.post(
-      Uri.parse('$baseUrl/briva'),
+      Uri.parse(baseUrl),
       headers: headers,
       body: data,
     );
+
+    print('Generated signature: $signature');
 
     print(response.body);
   } else {
